e61d62ff56e61b1dd5d829117138596735e17a30
Raphael Maia <raphzmaia@gmail.com>
2018-09-25 17:22:25 -0300

    mais de um objeto em tela para inicio da implementação de colisão, quando coloco em código estruturado so aparece 1

--- gdx-game/src/com/thousandeyes/TheLeak/State/PlayerIdleState.java
+++ gdx-game/src/com/thousandeyes/TheLeak/State/PlayerIdleState.java
@@ -7,11 +7,14 @@ public class PlayerIdleState implements IState
 	public PlayerIdleState(IGameObject gameObject){
 		GameObject = gameObject;
 		StateAnimation = AnimationHelper.GetAnimationFromSpritesheet("hero-idle-spritesheet.png",5,2,0.1f);
+		
 	}
 	@Override
 	public void Update(SpriteBatch batch, Float time)
 	{
 		batch.draw(StateAnimation.getKeyFrame(time, true), GameObject.Transform.x,GameObject.Transform.y, GameObject.Transform.width, GameObject.Transform.height);
+		Animation a = AnimationHelper.GetAnimationFromSpritesheet("hero-walking-spritesheet.png",5,2,0.1f);
+		batch.draw(a.getKeyFrame(time, true), 50,50, GameObject.Transform.width, GameObject.Transform.height);
 		
 	}
 
