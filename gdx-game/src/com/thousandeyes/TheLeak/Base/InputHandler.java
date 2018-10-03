cba6d19c7b58ac42a36f5237b600c0783f4f29a0
Raphael Maia <raphzmaia@gmail.com>
2018-10-03 08:39:59 -0300

    Input separado

--- gdx-game/src/com/thousandeyes/TheLeak/Base/InputHandler.java
+++ gdx-game/src/com/thousandeyes/TheLeak/Base/InputHandler.java
@@ -10,26 +10,40 @@ public class InputHandler
 	public static Vector2 InputVector(){
 		if(Gdx.input.isTouched())
 		{
-			if(InputHandler.OriginalTouch == null)
-			{
-				InputHandler.OriginalTouch = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-				return Vector2.Zero;
-			}
-			else
+			for (int i=0; i<5; i++)
 			{ 
-				Vector2 vec =new Vector2(Gdx.input.getX(), Gdx.input.getY()).sub(InputHandler.OriginalTouch);
-				vec.y *= -1;
-				if(Math.abs(vec.x) > 25f || Math.abs(vec.y) > 25f)
-					return vec.nor();
+				
+				if(InputHandler.OriginalTouch == null)
+				{
+					if(Gdx.input.getX(i)<= Gdx.graphics.getWidth() / 2)
+					{
+						InputHandler.OriginalTouch = new Vector2(Gdx.input.getX(i), Gdx.input.getY(i));
+						return Vector2.Zero;
+					}
+					else
+					{
+
+						InputHandler.OriginalTouch = null;
+						return Vector2.Zero;
+					}
+					
+				}
 				else
-					return Vector2.Zero;
+				{ 
+					Vector2 vec =new Vector2(Gdx.input.getX(i), Gdx.input.getY(i)).sub(InputHandler.OriginalTouch);
+					vec.y *= -1;
+					if(Math.abs(vec.x) > 25f || Math.abs(vec.y) > 25f)
+						return vec.nor();
+					else
+						return Vector2.Zero;
+				}
+				
 			}
+			
 		}
-		else
-		{
-			InputHandler.OriginalTouch = null;
-			return Vector2.Zero;
-		}
+		
+		InputHandler.OriginalTouch = null;
+		return Vector2.Zero;
 	};
 	private static Boolean actionPressed;
 	public static void setActionPressed(Boolean _actionPressed)
