28a8f305151b2a2cba5a0e1f443637e67cd2267e
Raphael Maia <raphzmaia@gmail.com>
2018-10-15 23:10:18 -0300

    Fix no follow da camera

--- gdx-game/src/com/thousandeyes/TheLeak/Entities/CameraHolder.java
+++ gdx-game/src/com/thousandeyes/TheLeak/Entities/CameraHolder.java
@@ -3,6 +3,7 @@ import com.thousandeyes.TheLeak.Base.*;
 import com.badlogic.gdx.graphics.*;
 import com.thousandeyes.TheLeak.State.*;
 import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.*;
 
 public class CameraHolder extends GameObject
 {
@@ -30,9 +31,11 @@ public class CameraHolder extends GameObject
 		Vector2 vec = new Vector2(x,camy);
 		Vector2 camvec = new Vector2(camx,camy);
 		
-		Vector2 cammov = vec.sub(camvec).nor();
-			
-		GameResources.Camera.position.set(camx + cammov.x * 10f,720f/2f,10f);
+		Vector2 cammov = vec.sub(camvec);
+	
+			GameResources.Camera.position.set(camx + cammov.x * Gdx.graphics.getDeltaTime(),720f/2f,10f);
+		
+		
 		GameResources.Camera.update();
 	}
 	
