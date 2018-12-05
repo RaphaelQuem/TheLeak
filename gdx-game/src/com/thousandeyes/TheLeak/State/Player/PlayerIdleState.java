d84ae68eb7f1955f36c662987f80396b88e87322
Raphael Maia <raphzmaia@gmail.com>
2018-12-05 19:30:50 -0200

    Mudanças aparência

--- gdx-game/src/com/thousandeyes/TheLeak/State/Player/PlayerIdleState.java
+++ gdx-game/src/com/thousandeyes/TheLeak/State/Player/PlayerIdleState.java
@@ -36,7 +36,7 @@ public class PlayerIdleState implements IState
 	
 	public PlayerIdleState(GameObject _gameObject){
 		gameObject = _gameObject;
-		stateAnimation = AnimationHelper.GetAnimationFromSpritesheet("hero-idle-spritesheet.png",3,2,0.16f);
+		stateAnimation = AnimationHelper.GetAnimationFromSpritesheet("hero-idle-spritesheet.png",3,2,0.08f);
 		GameResources.debugme = TextureRegionHelper.getAspectRatio("hero-idle-spritesheet.png",3,2);
 		//TextHelper.Show(String.valueOf(test),new Transform(0,0,10,10),3,3);
 		name = this.getClass().getName();
