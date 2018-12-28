dc7bc3f560d91989d317f0d0ab0b8fb9ad14400d
Raphael Maia <raphzmaia@gmail.com>
2018-12-28 18:51:26 -0200

    Construtor de animation com número de sprites específico

--- gdx-game/src/com/thousandeyes/TheLeak/Base/AnimationHelper.java
+++ gdx-game/src/com/thousandeyes/TheLeak/Base/AnimationHelper.java
@@ -7,6 +7,24 @@ import com.badlogic.gdx.assets.*;
 import com.thousandeyes.TheLeak.State.GameState.*;
 public class AnimationHelper
 {
+	public static Animation GetAnimationFromSpritesheet(String path, int cols, int rows, float time, int spriteCount){
+		Texture sheet = AssetHelper.getTexture(path);
+		
+		TextureRegion[][] tmp = TextureRegion.split(sheet, sheet.getWidth() / cols, sheet.getHeight() / rows);
+		TextureRegion[] frames = new TextureRegion[spriteCount];
+		int index = 0;
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				if(spriteCount ==0 || index<spriteCount)
+					frames[index++] = tmp[i][j];
+				
+			}
+		}
+		
+		return new Animation(time, frames);
+	}
 	public static Animation GetAnimationFromSpritesheet(String path, int cols, int rows, float time){
 		Texture sheet = AssetHelper.getTexture(path);
 
@@ -17,10 +35,11 @@ public class AnimationHelper
 		{
 			for (int j = 0; j < cols; j++)
 			{
-				frames[index++] = tmp[i][j];
+					frames[index++] = tmp[i][j];
+
 			}
 		}
-		
+
 		return new Animation(time, frames);
 	}
 	
--- gdx-game/src/com/thousandeyes/TheLeak/State/Player/PlayerWalkingState.java
+++ gdx-game/src/com/thousandeyes/TheLeak/State/Player/PlayerWalkingState.java
@@ -40,7 +40,7 @@ public class PlayerWalkingState implements IState
 
 	public PlayerWalkingState(GameObject _gameObject){
 		gameObject = _gameObject;
-		stateAnimation = AnimationHelper.GetAnimationFromSpritesheet("hero-walking-spritesheet.png",3,2,0.1f);
+		stateAnimation = AnimationHelper.GetAnimationFromSpritesheet("hero-walking-spritesheet.png",3,3,0.1f,8);
 		name = this.getClass().getName();
 		stateTime = 0f;
 	}
