996dd2d96926e0f5abc05c1115f8d14e1e1ed195
Raphael Maia <raphzmaia@gmail.com>
2019-01-10 18:54:39 -0200

    sprite

--- gdx-game/src/com/thousandeyes/TheLeak/Entities/MapLayer.java
+++ gdx-game/src/com/thousandeyes/TheLeak/Entities/MapLayer.java
@@ -2,7 +2,7 @@ package com.thousandeyes.TheLeak.Entities;
 import com.badlogic.gdx.graphics.*;
 public class MapLayer
 {
-	private float paralaxSpeed; 
+	private float paralaxSpeed;
 	private Texture texture;
 	private float positionX;
 
